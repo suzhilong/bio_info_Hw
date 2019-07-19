715fab36780ed3a9969d89a8bb35e536447a2117
SOUSIC <517048082@qq.com>
2019-07-19 09:07:54 +0800

    Add files via upload

--- project/ML_bio_hw.py
+++ project/ML_bio_hw.py
@@ -25,31 +25,33 @@ def data_prepro(data):
 def train(init_params, X, y, alpha, iterations, n=1):
 	'''
 	trainning model
-	There are three classfy mathods
+	There are three classfy methods
 	n=1, logistic
 	n=2, SVM
 	n=3, neural network
-	Save and return parameters of model
+	Save and return parameters of model and every iteration J
 	'''
 	if n == 1:
-		logistic_gradientDescent(params, X, y, alpha, iterations)
+		params, J_trace = logistic_gradientDescent(params, X, y, alpha, iterations)
+		return params, J_trace#every iteration of function J
 
-def classify(params, X):
+def classify(params, X, n=1):
 	'''
 	classify
 	'''
-	z = np.dot(X, params)#shape[M,1]
-	h = sigmoid(z)#[M,1]
-	y = np.zeros((X.shape[0],1))
-	for i in range(h.shape[0]):
-		y[i] = 1 if h[0] >= 0.5 else 0
-	return y#[M,1]
+	if n==1:#logistic
+		z = np.dot(X, params)#shape[M,1]
+		h = sigmoid(z)#[M,1]
+		# y = np.zeros((X.shape[0],1))
+		# for i in range(h.shape[0]):
+		# 	y[i] = 1 if h[0] >= 0.5 else 0
+		y = np.round(h)#easy method
+		return y#[M,1]
 
-def precise(params, X, y):
+def precise(y, y_predict):
 	'''
 	return precise of predict
 	'''
-	y_predict = classify(params, X)#[M,1]
 	same = 0
 	M = y.shape[0]
 	for i in range(M):
@@ -58,13 +60,15 @@ def precise(params, X, y):
 	precise = same/M
 	return precise	
 
-#logistic
+	
+
+#######logistic
 def sigmoid(z):
 	s = 1/(1+np.exp(-z))
 	return s
-def costFun(params, X, y):
+def costFunc(params, X, y):
 	'''
-	cost function
+	cost function —— no regularation
 	X shape is [M,N], N is features number, M is samples number
 	y shape is [M,1]
 	params shape is [N,1]
@@ -90,28 +94,35 @@ def logistic_gradientDescent(params, X, y, alpha, iterations):
 	for i in range(iterations):
 		grad = gradient(params, X, y)
 		params = params - alpha*grad
-		J_trace[i] = costFun(params, X, y)
+		J_trace[i] = costFunc(params, X, y)
 	return params, J_trace
+#######logistic
+
 
 def main():
 #get_data
 	path_train = ''
 	path_test = ''
 	path_val = ''
-	x_train_origin,y_train = get_data(path_train)
-	x_test_origin,x_test = get_data(path_test)
-	x_val_origin,x_val = get_data(path_val)
+	X_train_origin,y_train = get_data(path_train)
+	X_test_origin,y_test = get_data(path_test)
+	#x_val_origin,x_val = get_data(path_val)
 
 #data preprocessing
-	x_train =data_prepro()
-	x_test = data_prepro()
-	x_val = data_prepro()
+	X_train =data_prepro()
+	X_test = data_prepro()
+	#x_val = data_prepro()
 
+#1:logistic 2:SVM 3:neural network
+	n = 1
 #train
-	params = train(x_train)
+	init_params = np.zeros(X_train.shape[0])
+	params, J = train(init_params, X_train, y_train, alpha, iterations, n)
 
 #classification
-	y = classify(params, x_test)
+	y_predict = classify(params, X, n)
+	precise = precise(y_test, y_predict)
+	print('The precise of predict is:',precise)
 
 if __name__ == '__main__':
 	main()
